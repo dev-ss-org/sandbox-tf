ac85186d8a338a83fd8fba27e3ab533eb32a85ec
shotaro-suzuki <39366569+l-1cm-l@users.noreply.github.com>
2026-05-03 15:56:41 +0900

    Remove test S3 bucket resources from Terraform config

--- s3.tf
+++ s3.tf
@@ -1,13 +1,1 @@
-resource "aws_s3_bucket" "test" {
-  bucket           = "testbucket202605031523-169049008132-ap-northeast-1-an"
-  bucket_namespace = "account-regional"
-}
-
-resource "aws_s3_bucket_public_access_block" "test" {
-  bucket = aws_s3_bucket.test.id
-
-  block_public_acls       = true
-  block_public_policy     = true
-  ignore_public_acls      = true
-  restrict_public_buckets = true
-}
+# S3 bucket resources removed per PR follow-up request.
