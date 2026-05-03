62fc9172530fd0204bac8374706109e8be554e57
lー1cmーl <39366569+l-1cm-l@users.noreply.github.com>
2026-05-03 07:55:56 +0000

    chore: add account-regional bucket

--- s3.tf
+++ s3.tf
@@ -3,13 +3,15 @@ data "aws_caller_identity" "current" {}
 data "aws_region" "current" {}
 
 resource "aws_s3_bucket" "regional_namespace_bucket" {
-  bucket = "${data.aws_caller_identity.current.account_id}-${data.aws_region.current.name}-${random_string.bucket_suffix.result}"
+  bucket           = "sample-${data.aws_caller_identity.current.account_id}-${data.aws_region.current.name}-an"
+  bucket_namespace = "account-regional"
 }
 
-resource "random_string" "bucket_suffix" {
-  length  = 8
-  lower   = true
-  numeric = true
-  special = false
-  upper   = false
+resource "aws_s3_bucket_public_access_block" "regional_namespace_bucket" {
+  bucket = aws_s3_bucket.regional_namespace_bucket.id
+
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
 }
