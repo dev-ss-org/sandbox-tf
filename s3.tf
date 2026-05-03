16f9d381ee859f9c9b13b74e1942fe6925169517
lー1cmーl <39366569+l-1cm-l@users.noreply.github.com>
2026-05-03 06:25:15 +0000

    feat: add test-S3

--- s3.tf
+++ s3.tf
@@ -1,1 +1,13 @@
+resource "aws_s3_bucket" "test" {
+  bucket           = "testbucket202605031523-169049008132-ap-northeast-1-an"
+  bucket_namespace = "account-regional"
+}
 
+resource "aws_s3_bucket_public_access_block" "test" {
+  bucket = aws_s3_bucket.test.id
+
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+}
